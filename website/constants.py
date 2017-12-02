94af59e5ac7708064157f87e4248ea598a16a4ab
Ramsey <ramsey.b.harrison@gmail.com>
2017-12-02 11:50:42 -0500

    retardation:

---
+++ website/constants.py
@@ -0,0 +1,5 @@
+MEAL = (('BR', 'Breakfast'),
+        ('LU', 'Lunch'),
+        ('DI', 'Dinner'),
+        ('OT', 'Other')
+        )
--- website/models.py
+++ website/models.py
@@ -2,14 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from constants import MEAL
 # Create your models here.
 
 class foodItem(models.Model):
     name = models.TextField()
     description = models.CharField(max_length=50)
     location = models.CharField(max_length=200,default="east")
-
+    meal = models.CharField(max_length=2, choices=MEAL, blank=False,default="OT")
 
 class foodItemComment(models.Model):
     foodItem = models.ForeignKey(foodItem)
